6bde344cd1422fde59a8eac4c8b3423a384247af
JAPH254 <omanyujapheth@gmail.com>
2024-03-07 22:56:10 +0300

    dart resources

---
+++ Dart_Variables/variables.dart
@@ -0,0 +1,29 @@
+//dart variables
+void main() {
+  var name = "John";
+  var age = 25;
+  var height = 1.75;
+  var isStudent = false;
+  print(name);
+  print(age);
+  print(height);
+  print(isStudent);
+  //print a concatenated string
+  print("My name is $name, I am $age years old, and I am $height meters tall.");
+
+
+// declaring variables
+String me = "John"; //must be in quotes
+String address = "Kenya";  //must be in quotes
+num age2 = 20; //whole numbers
+num heightt = 5.9;//decimal numbers
+bool isMarried = false; //boolean
+   
+// printing variables value   
+print("Name is $me");
+print("Address is $address");
+print("Age is $age2");
+print("Height is $heightt");
+print("Married Status is $isMarried");
+
+}
